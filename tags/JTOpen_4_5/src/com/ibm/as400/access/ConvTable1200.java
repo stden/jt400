218aedf1603313b6997dc691bb6137ab38f8b3e6
nobody <nobody@7fc04f9b-07c3-4dc2-9388-7e3d9ac30f30>
2004-08-02 17:05:12 +0000

    This commit was manufactured by cvs2svn to create tag 'JTOpen_4_5'.

---
+++ tags/JTOpen_4_5/src/com/ibm/as400/access/ConvTable1200.java
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// JTOpen (IBM Toolbox for Java - OSS version)
+//
+// Filename:  ConvTable1200.java
+//
+// The source code contained herein is licensed under the IBM Public License
+// Version 1.0, which has been approved by the Open Source Initiative.
+// Copyright (C) 2004 International Business Machines Corporation and
+// others.  All rights reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.ibm.as400.access;
+
+class ConvTable1200 extends ConvTableUnicodeBigMap 
+{
+    private static final String copyright = "Copyright (C) 2004 International Business Machines Corporation and others.";
+
+    ConvTable1200()
+    {
+        super(1200);
+    }
+}
